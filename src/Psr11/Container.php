44455a7604560944505c2a7fd1f9ceafd18bf0d5
ebcms <1540837821@qq.com>
2020-06-04 20:06:52 +0800

    1

--- src/Psr11/Container.php
+++ src/Psr11/Container.php
@@ -1,6 +1,6 @@
 <?php declare (strict_types = 1);
 
-namespace Psr11;
+namespace Ebcms\Psr11;
 
 use Exception;
 use Psr11\Exception\ContainerException;
--- src/Psr11/Exception/ContainerException.php
+++ src/Psr11/Exception/ContainerException.php
@@ -1,6 +1,6 @@
 <?php declare (strict_types = 1);
 
-namespace Psr11\Exception;
+namespace Ebcms\Psr11\Exception;
 
 use Psr\Container\ContainerExceptionInterface;
 
